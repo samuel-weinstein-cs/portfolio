ed052ef225582ca047ef240ea5d9387e0f7e60e9
sweinstein <raeweinst@gmail.com>
2020-03-02 13:07:30 -0700

    add basic header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Sam Weinstein</h1>
+          <nav>
+            <span>Projects</span>
+            <span>About</span>
+            <span>Skills</span>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
