2fdc59b78b25c139c34942bdaf0f30bae61f84fc
sweinstein <raeweinst@gmail.com>
2020-03-11 09:18:00 -0400

    finish icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import c from '../icons/c/c-original.svg';
+import cplusplus from '../icons/cplusplus/cplusplus-original.svg';
+import css3 from '../icons/css3/css3-original-wordmark.svg';
+import git from '../icons/git/git-original-wordmark.svg';
+import github from '../icons/github/github-original-wordmark.svg';
+import heroku from '../icons/heroku/heroku-original-wordmark.svg';
+import html5 from '../icons/html5/html5-original-wordmark.svg';
+import java from '../icons/java/java-original-wordmark.svg';
+import javascript from '../icons/javascript/javascript-original.svg';
+import linux from '../icons/linux/linux-original.svg';
+import nodejs from '../icons/nodejs/nodejs-original-wordmark.svg';
+import npm from '../icons/npm/npm-original-wordmark.svg';
+import postgresql from '../icons/postgresql/postgresql-original-wordmark.svg';
+import python from '../icons/python/python-original-wordmark.svg';
+import rails from '../icons/rails/rails-original-wordmark.svg';
+import react from '../icons/react/react-original-wordmark.svg';
+import ruby from '../icons/ruby/ruby-original-wordmark.svg';
+import sequelize from '../icons/sequelize/sequelize-original-wordmark.svg';
 
 const Skills = () => {
   return (
     <div className="skills-container">
-      <img src="../icons/c/c-original.svg" />
+      <img src={c} />
+      <img src={cplusplus} />
+      <img src={css3} />
+      <img src={git} />
+      <img src={github} />
+      <img src={heroku} />
+      <img src={html5} />
+      <img src={java} />
+      <img src={javascript} />
+      <img src={linux} />
+      <img src={nodejs} />
+      <img src={npm} />
+      <img src={postgresql} />
+      <img src={python} />
+      <img src={rails} />
+      <img src={react} />
+      <img src={ruby} />
+      <img src={sequelize} />
+
+
     </div>
   )
 }
