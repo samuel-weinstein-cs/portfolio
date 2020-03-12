66952ace37d44669279092ed403ad8eacdc5d322
sweinstein <raeweinst@gmail.com>
2020-03-12 09:15:32 -0400

    add some projects

--- src/App.js
+++ src/App.js
@@ -19,12 +19,25 @@ class App extends Component {
           image:"https://i.imgur.com/9HMU1Hc.png",
           url:"http://wordsperminute.surge.sh"
         },
+        {
+          title:"Open Movie Database",
+          description:"Open Movie database is an app where users can search a database of movies, tv shows, and video games. The app then displays information about the movie. This project uses an API and React.js.",
+          image:"https://i.imgur.com/31rP9Pm.png",
+          url:"http://openmoviedatabase.surge.sh"
+        },
+        {
+          title:"Evently",
+          description:"Evently is a website designed for allowing users to create, view, and attend events in their local area. Users can access a database of events created by other users and notify others that they are attending an event.",
+          image:"https://i.imgur.com/TO64xuD.png",
+          url:"http://eventlynyc.surge.sh"
+        },
+
       ]
     }
   }
 
   componentDidMount(){
-    // this.props.history.push(`/${this.state.page}`)
+    console.log(this.props.location.pathname);
   }
 
   render(){
@@ -77,6 +90,7 @@ class App extends Component {
                 ))
               }
             </Route>
+
           </Switch>
         </main>
 
